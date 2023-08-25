3a2535f8ccee04e507932cceff92afc88d4e8e8c
sangeetakuntoji <sangeeta.kuntoji@gmail.com>
2023-08-25 23:06:00 +0200

    updated  yml

---
+++ TF_webapp/variable.tf
@@ -0,0 +1,11 @@
+variable "name" {
+  type        = string
+  default     = "devlopment"
+  description = "Name for resources"
+}
+
+variable "location" {
+  type        = string
+  default     = "uksouth"
+  description = "Azure Location of resources"
+}
